e7bdec0baedaebd9f548b6e6bf1e100abf904d20
Christine Groux <christinegroux1@gmail.com>
2025-03-31 18:12:43 +0200

    feat(database): init docker files and initiate table

---
+++ init/init_nips_tags.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS nips_tag_current (
+    nip VARCHAR(255) PRIMARY KEY NOT NULL,
+    final BOOLEAN NOT NULL DEFAULT false,
+    draft BOOLEAN NOT NULL DEFAULT false,
+    mandatory BOOLEAN NOT NULL DEFAULT false,
+    optional BOOLEAN NOT NULL DEFAULT false,
+    recommended BOOLEAN NOT NULL DEFAULT false,
+    unrecommended BOOLEAN NOT NULL DEFAULT false,
+    last_updated DATE NOT NULL
+);
+
