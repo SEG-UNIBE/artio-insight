f9d30cc6e6e09d09e5f9129f6c9648a1434fbaad
Christine Groux <christinegroux1@gmail.com>
2025-03-31 22:34:40 +0200

    feat(database): fill table with data

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
-	"artio-insight/pkg/scripts"
+	m "artio-insight/models"
+	s "artio-insight/pkg/scripts"
 	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	g "gorm.io/gorm"
 )
 
 func main() {
-	tags,_ := scripts.GetCurrentNipsTags()
+	db, err := g.Open(postgres.New(postgres.Config{
+		DSN: "user=postgres password=admin dbname=artio_db host=localhost port=5432 sslmode=disable TimeZone=Europe/Zurich",
+		PreferSimpleProtocol: true,
+	}), &g.Config{})
+
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+	fmt.Println("Connected to database")
+
+	tags,_ := s.GetNipsTags()
+	currentTime := time.Now()
 
 	for nip, tag := range tags {
-		fmt.Println(nip, ":", tag)
+		nipTags,_ := m.GetCurrentNipTags(nip, currentTime, tag)
+
+		result := db.Create(&nipTags)
+		if result.Error != nil {
+			fmt.Println("Error inserting data:", result.Error)
+		}
 	}
+
+	fmt.Println("\nData inserted successfully")
 }
