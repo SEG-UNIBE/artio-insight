9dfc8be6144d614b488c7071b36300291e807ecf
Christine Groux <christinegroux1@gmail.com>
2025-03-31 16:24:12 +0200

    feat(nip-tags): add error handling and comments

--- pkg/scripts/nip-tags.go
+++ pkg/scripts/nip-tags.go
@@ -10,10 +10,13 @@ import (
 )
 
 const (
+	// URL of the GitHub repository where the NIPs documentation is stored
 	REPO_URL = "https://github.com/nostr-protocol/nips"
+	// URL prefix for fetching the raw markdown files of the NIPs
 	RAW_FILES_URL = "https://raw.githubusercontent.com/nostr-protocol/nips/refs/heads/master/"
 )
 
+// Takes in a file name and checks if it is (probably) a NIP file
 func isNip(lookup string) bool {
 	switch lookup {
 	case
@@ -27,61 +30,84 @@ func isNip(lookup string) bool {
 	return true
 }
 
-func getNips() []string {
-	collector := colly.NewCollector()
-	collector.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Error:", e)
-	})
-
+// Fetches the list of files in the github repo and extracts the NIPs
+func getNips() ([]string, error) {
 	nips := []string{}
 
+	collector := colly.NewCollector()
 	collector.OnHTML(".react-directory-filename-cell", func(e *colly.HTMLElement) {
 		if isNip(e.Text) {
-			nips = append(nips, e.Text)
+			nip := e.Text[:len(e.Text)-len(".md")]
+			nips = append(nips, nip)
 		}
 	})
 
-	collector.Visit(REPO_URL)
+	err := collector.Visit(REPO_URL)
 
-	return nips
+	return nips, err
 }
 
-func getTagsFromText(text string) []string {
-	tags := []string{}
-
-	if idx := regexp.MustCompile(`(?m)^##`).FindStringIndex(text); idx != nil {
-		text = text[:idx[0]]
+// Reads the raw markdown of a NIP file and returns the extracted tags
+func getTagsFromText(text string) ([]string, error) {
+	if text == "" {
+		return nil, fmt.Errorf("empty text provided")
 	}
 
-	re := regexp.MustCompile("(?:`\\w+`\\s*)+\n")
-	line_match := re.FindString(text)
+	tags := []string{}
+
+	// Finds the line with the tags (i.e. line with only tags)
+	re := regexp.MustCompile("\n(?:`[a-zA-Z]+`\\s*)+\n")
+	line_matches := re.FindAllString(text, -1)
+
+	if len(line_matches) > 1 {
+		fmt.Println(line_matches)
+		return nil, fmt.Errorf("multiple lines with possible tags found")
+	}
+
+	if len (line_matches) == 0 {
+		return nil, nil
+	}
+
+	// Extracts the tags from the line
 	re_words := regexp.MustCompile("`\\w+`")
-	matches := re_words.FindAllString(line_match, -1)
+	matches := re_words.FindAllString(line_matches[0], -1)
 
 	tags = append(tags, matches...)
 
-	return tags
+	return tags, nil
 }
 
-func GetCurrentNipsTags() map[string][]string {
+// Extracts the tags from the NIPs in the GitHub repository
+func GetCurrentNipsTags() (map[string][]string, error) {
 	nip_texts := map[string][]string{}
 	var tmp_text string
+	nips, err := getNips()
 
-	for _, nip := range getNips() {
-		res, err := http.Get(RAW_FILES_URL + nip)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching NIPs: %w", err)
+	}
+
+	for _, nip := range nips {
+		nip_texts[nip] = []string{}
+		res, err := http.Get(RAW_FILES_URL + nip + ".md")
 		if err != nil {
-			fmt.Println("Error fetching file:", err)
+			fmt.Println("error fetching file", nip, " :", err)
 			continue
 		}
 		content, err := io.ReadAll(res.Body)
 		res.Body.Close()
 		if err != nil {
-			fmt.Println("Error reading response body:", err)
+			fmt.Println("error reading response body for", nip, " :", err)
 			continue
 		}
 		tmp_text = string(content)
-		nip_texts[nip] = getTagsFromText(tmp_text)
+		tags, err := getTagsFromText(tmp_text)
+		if err != nil {
+			fmt.Println("error extracting tags for", nip, " :", err)
+			continue
+		}
+		nip_texts[nip] = tags
 	}
 
-	return nip_texts
+	return nip_texts, nil
 }
