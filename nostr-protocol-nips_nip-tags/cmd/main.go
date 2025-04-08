3dcd3e77fb4c3fa213ea3a16597d93e901f977d6
Christine Groux <christinegroux1@gmail.com>
2025-04-08 16:27:52 +0200

    fix: improve error logging in nip-tags main

--- nostr-protocol-nips_nip-tags/cmd/main.go
+++ nostr-protocol-nips_nip-tags/cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	m "nostr-protocol-nips_nip-tags/models"
 	s "nostr-protocol-nips_nip-tags/pkg/nipTags"
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 
 	"github.com/joho/godotenv"
 )
@@ -29,9 +31,17 @@ func main() {
 	for nip, tag := range tags {
 		nipTags,_ := m.ToCurrentNipTags(nip, currentTime, tag)
 
-		result := m.DB.Create(&nipTags)
-		if result.Error != nil {
-			log.Error("error while inserting tags of NIP-", nip, " : ", result.Error)
+		curr := m.DB.First(&m.CurrentNipTags{}, nip)
+		if errors.Is(curr.Error, gorm.ErrRecordNotFound) {
+			log.Info("new nip found : NIP-", nip)
+			result := m.DB.Create(&nipTags)
+			if result.Error != nil {
+				log.Error("error while inserting tags of new NIP-", nip, " : ", result.Error)
+			}
+			continue
+		} else if curr.Error != nil {
+			log.Error("could not check if nip ", nip, " exists in database: ", curr.Error)
+			continue
 		}
 	}
 
