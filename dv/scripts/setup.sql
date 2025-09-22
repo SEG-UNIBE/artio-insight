24af09de2aff35e7ee084a683bfc07529db33c13
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-09-22 22:38:18 +0200

    chore(artioinf): adding tables for new source system

--- dv/scripts/setup.sql
+++ dv/scripts/setup.sql
@@ -4,7 +4,7 @@ Author:				Michael Kaiser
 Create date:		2025-08-20
 Description:		Script to setup the needed database schemas and other structures for the insight DV.
 Modification:
-YYYY-MM-DD	mkaiser
+2025-09-22	mkaiser     Adding default roles for each schema (reader and admin)
 =============================================
 */
 
