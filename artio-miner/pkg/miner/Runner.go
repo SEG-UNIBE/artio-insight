e8efec2e7d35ddeb004b41d7054b9469a10bbb73
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-11-17 18:41:52 +0100

    fix(runner): fixing naming problem on runner

--- artio-miner/pkg/miner/Runner.go
+++ artio-miner/pkg/miner/Runner.go
@@ -21,9 +21,7 @@ type Runner struct {
 handleRelay process a single relay and store its information in the database
 */
 func (rnr *Runner) handleRelay(relay *RelayMiner) {
-
-	rnr.loadMap[relay.CleanName()] = true
-
+	rnr.SetLoadMapEntryTrue(relay.CleanName())
 	// load the relay information
 	relay.Load()
 	//relay.Stats()
@@ -63,7 +61,7 @@ func (rnr *Runner) handleRelay(relay *RelayMiner) {
 			rnr.Neo.Execute(`MERGE(r:Relay {name: $name, isValid: $isValid, validReason: $validReason})`, map[string]any{"name": newRelay.CleanName(), "validReason": newRelay.InvalidReason, "isValid": newRelay.IsValid})
 			// rnr.Neo.Execute(`MERGE(r:Relay {name: $name})`, map[string]any{"name": newRelay.CleanName()})
 
-			if rnr.loadMap[newRelay.CleanName()] {
+			if rnr.GetLoadMapEntry(newRelay.CleanName()) {
 				rnr.Neo.Execute(`MATCH(r1:Relay), (r2:Relay) WHERE r1.name=$name1 and r2.name=$name2 MERGE (r1)-[:DETECTED]->(r2);`, map[string]any{"name1": relay.CleanName(), "name2": newRelay.CleanName()})
 			}
 
