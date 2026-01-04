666b0a2c53359c625ab83c5d8f80bfd1bbd99535
Michael Kaiser <michael.kaiser@emplabs.ch>
2026-01-04 14:56:03 +0100

    feat(insight): continue work on notebook and fix udm scripts

--- dv/udm/RelaySoftware.sql
+++ dv/udm/RelaySoftware.sql
@@ -4,15 +4,16 @@ Author:				Michael Kaiser
 Create date:		2025-11-25
 Description:		Relationship table for relay to software usage
 Modification:
+2026-01-04  mkaiser     Fix the mix of software and owner
 =============================================
 */
 
-DROP TABLE IF EXISTS udm.RelayOwner;
+DROP TABLE IF EXISTS udm.RelaySoftware;
 
 SELECT *
-INTO udm.RelayOwner
+INTO udm.RelaySoftware
 FROM (SELECT 'neo4j' AS record_src,
-             _start  AS user_id,
-             _end    AS relay_id
+             _start  AS relay_id,
+             _end    AS software_id
       FROM rdv.dump_neo4j_sat_cur
-      WHERE _type = 'OWNS') AS src;
+      WHERE _type = 'USES_SOFTWARE') AS src;
